10d960aa45db99b3a0939abe567caa1ee1995c36
Akshay4394 <Akshaypatel4394@gmail.com>
2022-09-12 12:49:58 -0500

    this is my second commit after changes made

---
+++ src/gitdemo/gittest1.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+public class gittest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String akshay = "roshani";
+		
+		System.out.println(akshay);
+		
+		
+	}
+
+}
